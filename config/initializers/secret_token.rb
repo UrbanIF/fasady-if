ec46ef7f2ff2cb32c9cc7ac81056b238318a2c49
infernalmaster <infernalmaster1@gmail.com>
2013-10-26 13:28:24 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FasadyIf::Application.config.secret_key_base = '157a5806ea1b80ff41c6495ddb3165dfc9d14f0869066fb4be0cd60e68a36df23a2f4c32d1c8229833c256d021ffdcd7ae6aece1bcb2ed3b05bcca934de1ac8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FasadyIf::Application.config.session_store :cookie_store, key: '_fasady-if_session'
